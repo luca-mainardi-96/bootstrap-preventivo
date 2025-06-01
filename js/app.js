61c042f12d39dfc129a68432a462d1ffba12edfa
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-06-01 19:32:22 +0200

    html + base js

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// recupero elementi form
+
+const inputWork = document.getElementById('inputWork')
+const discounCode = document.getElementById('discountCode')
+const formEstimate = document.getElementById('form')
+const outputPrice = document.getElementById('outputPrice')
+//codici sconto
+
+const discountCodes = [
+    'YHDNU32',
+    'JANJC63',
+    'PWKCN25',
+    'SJDPO96',
+    'POCIE24'
+]
+//dichiarazione altre varibili
+
+const hoursOfWork = 10
+const backendPrice = 20.5
+const frontendPrice = 15.3
+const analysisPrice = 33.6
+const discountedPrice = 0.75
+
+//recupero evento
+
+formEstimate.addEventListener
+
+//calcolo preventivo
+
+if(inputWork.value = 'backend'){
+    outputPrice = 
+}
