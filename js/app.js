b6bc32a88a1f3afd0db5ed7c72caa4936a8576f9
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-06-02 16:26:15 +0200

    finito (da ricontrollare)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,41 @@
 // recupero elementi form
 
-const inputWork = document.getElementById('inputWork')
-const discounCode = document.getElementById('discountCode')
+const selectWork = document.getElementById('inputWork')
+const discountCode = document.getElementById('discountCode')
 const formEstimate = document.getElementById('form')
-const outputPrice = document.getElementById('outputPrice')
+let outputPrice = document.getElementById('outputPrice')
+let integerPart = document.getElementById('integerPart')
+let decimalPart = document.getElementById('decimalPart')
+const errorMessage = document.getElementById('codeError')
+
+// options dinamiche
+
+const options = [
+    {
+        value: '',
+        text: 'Seleziona il tipo di lavoro'
+    },
+    {
+        value: 'backend',
+        text: 'Backend Developement'
+    },
+    {
+        value: 'frontend',
+        text: 'Frontend Developement'
+    },
+    {
+        value: 'pAnalysis',
+        text: 'Project Analysis'
+    }
+]
+
+options.forEach((i) => {
+    const option = document.createElement('option');
+    option.value = i.value;
+    option.text = i.text;
+    selectWork.appendChild(option);
+});
+
 //codici sconto
 
 const discountCodes = [
@@ -19,14 +51,45 @@ const hoursOfWork = 10
 const backendPrice = 20.5
 const frontendPrice = 15.3
 const analysisPrice = 33.6
-const discountedPrice = 0.75
 
 //recupero evento
 
-formEstimate.addEventListener
+formEstimate.addEventListener('submit', function(e){
+    e.preventDefault()
 
-//calcolo preventivo
+let work = selectWork.value
+let price = 0
+const userCode = discountCode.value.trim().toUpperCase()
 
-if(inputWork.value = 'backend'){
-    outputPrice = 
+discountCode.classList.remove('is-invalid');
+errorMessage.classList.add('d-none');
+
+if(work === 'backend'){
+    price = backendPrice*hoursOfWork
+} else if (work === 'frontend'){
+    price = frontendPrice*hoursOfWork
+} else if (work === 'pAnalysis'){
+    price = analysisPrice*hoursOfWork
 }
+
+if (userCode !== "") {
+    if (discountCodes.includes(userCode)) {
+        price = price * 0.75;
+        discountCode.classList.remove('is-invalid');
+        errorMessage.classList.add('d-none');
+    } else {
+        discountCode.classList.add('is-invalid');
+        errorMessage.classList.remove('d-none');
+        return;
+    }
+}
+
+let formattedPrice = price.toFixed(2)
+
+const dividedNumber = formattedPrice.split('.')
+
+integerPart.innerHTML = "€ " + dividedNumber[0] + '.'
+decimalPart.innerHTML = dividedNumber [1]
+})
+
+
